e07bcd7b0ffd03fa6f2ea7404d096ffbc3d852ec
Facundo Rodriguez <fr8080342@gmail.com>
2025-02-27 11:11:38 -0300

    probando variables dinamicas segun el ambiente

--- client/app.ts
+++ client/app.ts
@@ -1,3 +1,6 @@
-fetch('http://localhost:8000/')
+let url = process.env.RENDER_EXTERNAL_URL + '/messi' || 'http://localhost:8000/messi';
+console.log(url);
+// let url = process.env.RENDER_EXTERNAL_URL + '/newMenssaje';
+fetch(url)
 .then( res => { return res.text() } )
 .then( data => { console.log(data) } )
--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 
 let app = express();
 app.use(express.static('dist'));
+let port = process.env.PORT || 8000;
+console.log('====> ', port);
 
 app.get('/messi', (req, res) => { 
     console.log('nueva solicitud');
     res.send('/index.js') } );
     
-app.listen(8000, () => { console.log('server encendido...') } )
+app.listen(port, () => { console.log('server encendido...') } )
